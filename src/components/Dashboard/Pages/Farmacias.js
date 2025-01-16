a288d0deaade3a857c01f58264ae87d761ebfb2a
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-01-15 23:30:20 -0300

    adding new drugstores

--- src/components/Dashboard/Pages/Farmacias.js
+++ src/components/Dashboard/Pages/Farmacias.js
@@ -30,12 +30,11 @@ export default function Farmacias(){
         <div style={{position: 'absolute', zIndex: 1000}}>
           <Sidebar activeDirectory={"farmacias"}/>
         </div>
-        <div style={{margin: '2rem 2rem 2rem 7rem', width: '100%'}}>
-          <p style={{fontSize: '2.5rem', fontWeight: '300', marginBottom: '2rem'}}>Aproveite a nossa parceria com diversas farmácias</p>
-          <Row style={{gap: '2rem', display: 'flex'}}>
+        {/* <div style={{margin: '2rem 2rem 2rem 7rem', width: '100%'}}> */}
+          {/* <Row style={{gap: '2rem', display: 'flex'}}>
             <Col>
-              <Link className='farmacias-link'>
-                <Card style={{ width: '15rem' }}>
+            <Link className='farmacias-link'>
+            <Card style={{ width: '15rem' }}>
                     <Card.Img variant="top" style={{width: '100%', padding: '2rem 2rem 0.5rem 2rem', height: '10rem', objectFit: 'contain'}} src="/drogasmil.png" />
                   <Card.Body>
                     <Card.Title>Drogasmil</Card.Title>
@@ -66,8 +65,199 @@ export default function Farmacias(){
                 </Card>
               </Link>
             </Col>
-          </Row>
-        </div>
+          </Row> */}
+
+          <div style={{ margin: '2rem 2rem 2rem 7rem', width: '100%' }}>
+            <p style={{fontSize: '2.5rem', fontWeight: '300', marginBottom: '2rem'}}>Aproveite a nossa parceria com diversas farmácias</p>
+            
+            <Row style={{ justifyContent: 'center', gap: '2rem' }}>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/pacheco.png"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>Pacheco</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 15% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 25% de desconto*
+                      <br />
+                      5% de desconto para OTC (Medicamentos Isentos de Prescrição)
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/drogariaSaoPaulo.png"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>Drogaria São Paulo</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 15% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 25% de desconto*
+                      <br />
+                      5% de desconto para OTC (Medicamentos Isentos de Prescrição)
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/drogariasTamoio.png"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>Tamoio</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 10% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 10% de desconto*
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/drogasmil.png"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>Drogasmil</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 10% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 10% de desconto*
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/farmalife.jpg"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>FarmaLife</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 10% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 10% de desconto*
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/drogariaRosario.png"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>Rosário</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 10% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 10% de desconto*
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/pagueMenos.png"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>Pague Menos</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 15% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 25% de desconto*
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={6} md={6} lg={3}>
+                <Card style={{ width: '100%', height: '100%' }}>
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      padding: '2rem',
+                      height: '10rem',
+                      objectFit: 'contain',
+                    }}
+                    src="/extraFarma.png"
+                  />
+                  <Card.Body style={{ flex: 1 }}>
+                    <Card.Title>Extrafarma</Card.Title>
+                    <p>
+                      Medicamentos Tarjados de Marca: No mínimo 15% de desconto*
+                      <br />
+                      Medicamentos Tarjados Genéricos: No mínimo 25% de desconto*
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+
+
+
+
+        {/* </div> */}
       </div>
     )
 }
